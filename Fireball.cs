e53e75894dcc880d1e4ed613e2764a9d1df87ece
San-Atractivo <sky900520@naver.com>
2018-08-15 10:14:19 +0900

    ChoppaRunGame Source

---
+++ Fireball.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : ObjectPool {
+
+    Animator ani;
+    AudioSource audio;
+    private float speed = 6.5f;
+
+    bool ui;
+
+    private void Awake()
+    {
+        ani = GetComponent<Animator>();
+        audio = GetComponent<AudioSource>();
+    }
+
+    private void Start()
+    {
+        ani = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (transform.position.x < -5)
+            removeObject();
+        transform.position = new Vector3(transform.position.x - (speed * Time.deltaTime * GameManager.SingleTon.GetGameSpeed()), transform.position.y, transform.position.z);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag.Equals("Player"))
+        {
+            bool death;
+            
+            audio.Play();
+            ani.SetTrigger("Explosion");
+            death = col.gameObject.transform.parent.GetComponent<Player>().PlayerDead();
+            if (death) audio.Play();
+            GetComponent<CircleCollider2D>().enabled = false;
+        }
+    }
+
+    public void removeObject()
+    {
+        puspAction(this.gameObject);
+    }
+
+    private void OnDisable()
+    {
+        GetComponent<CircleCollider2D>().enabled = true;
+    }
+}
---
+++ Utills.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utills {
+
+    public static string NumberToCommaText(int value)
+    {
+        return string.Format("{0:#,##0}", value);
+    }
+
+    public static string GetNowDateTime()
+    {
+        return System.DateTime.Now.ToString("yyyy/MM/dd HH:mm");
+    }
+
+    public static string DateTimeToString(System.DateTime time)
+    {
+        return time.ToString("yyyy/MM/dd HH:mm");
+    }
+}
